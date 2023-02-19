94f4623bc6db9fcaeb1613dbf838c348d213cf4a
Javi Villar <javier.villar@makingscience.com>
2023-02-19 21:13:39 +0100

    init repo and project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        
+      </main>
+    </div>
+  )
+}
+
+export default App
