6ef90103c09b2afed85355800c5674831f2c464a
Javi Villar <javier.villar@makingscience.com>
2023-02-25 20:31:40 +0100

    feed videos to map all videos

--- src/components/FeedVideos/index.tsx
+++ src/components/FeedVideos/index.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import VideoPlayer from '../VideoPlayer';
+import styles from './styles.module.css';
 
-export default function FeedVideos () {
 
+const VIDEOS = [
+    {
+        id: 'arst1234',
+        src: 'https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-fashion-woman-with-silver-makeup-39875-large.mp4',
+        userName: 'Lorem Ipsum',
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        song: '',
+        albumCover: '',
+        numComments: '',
+        likes: 40,
+        shares: 13,
+    },
+    {
+        id: '1234',
+        src: 'https://assets.mixkit.co/videos/preview/mixkit-man-holding-neon-light-1238-large.mp4',
+        userName: 'Dolor Sit',
+        description: 'Excepteur sint occaecat cupidatat non proident',
+        song: '',
+        albumCover: '',
+        numComments: '',
+        likes: 64,
+        shares: 23,
+    },
+]
+export default function FeedVideos() {
+    return (
+        <>
+            {VIDEOS.map(v => <VideoPlayer key={v.id} {...v} />)}
+        </>
+    )
 }
