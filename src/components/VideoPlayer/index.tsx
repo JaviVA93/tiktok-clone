5a2a74b230954ae8ab6e3c18d3e8ecf081f7f16e
Javi Villar <javier.villar@makingscience.com>
2023-02-25 18:15:17 +0100

    show/hide player button, handlePlay on wrapper, video loop

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -7,26 +7,37 @@ const videoSrc = 'https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-f
 export default function VideoPlayer() {
     const [playing, setPlaying] = useState(false);
     const video = useRef<HTMLVideoElement>(null);
+    const playerBtn = useRef<HTMLButtonElement>(null);
+
     const handlePlay = () => {
         if (playing) {
             video.current?.pause();
             setPlaying(false);
+            playerBtn.current?.classList.remove(styles.hidden);
         }
         if (!playing) {
             video.current?.play();
             setPlaying(true);
+            playerBtn.current?.classList.add(styles.hidden);
         }
     }
-    
+
     return (
-        <div className={styles.wrapper}>
+        <div
+            className={styles.wrapper}
+            onClick={handlePlay} >
             <video
                 ref={video}
                 className={styles.video}
                 src={videoSrc}
                 controls={false}
+                loop
             />
-            <button type='button' className={styles.playBtn} onClick={handlePlay}></button>
+            <button
+                ref={playerBtn}
+                type='button'
+                className={styles.playBtn}>
+            </button>
         </div>
     )
 }
