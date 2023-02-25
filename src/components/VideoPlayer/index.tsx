3b1fdf4a8ccf0ea61f66184af2af720acb70a1ec
Javi Villar <javier.villar@makingscience.com>
2023-02-25 18:06:36 +0100

    play/pause video behavior

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,8 +1,32 @@
-const videoSrc = 'https://v16-webapp-prime.tiktok.com/video/tos/useast2a/tos-useast2a-pve-0068/osf8JQionLOriBK9QDbxA6gcLe8zkjBE1CoRUy/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2730&bt=1365&cs=0&ds=3&ft=I~da4od2D12Nv1kJzZIxRtr_YlJG-UjNSmgpi9&mime_type=video_mp4&qs=0&rc=aDs7NzVmaGdnNmY0Nzc3PEBpM3hwOTQ6ZnI1aTMzNzczM0BiNWA2Ni9hNmAxXjY2MjFhYSNuX2BjcjRnaW1gLS1kMTZzcw%3D%3D&btag=80000&expire=1676858233&l=2023021919552184124644BFEA80C9894C&ply_type=2&policy=2&signature=4628cb91a446b84f3e940266a8021ca3&tk=tt_chain_token'
+import styles from './styles.module.css';
+import { useRef, useState } from 'react';
+
+const videoSrc = 'https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-fashion-woman-with-silver-makeup-39875-large.mp4'
 
 
-export default function VideoPlayer () {
+export default function VideoPlayer() {
+    const [playing, setPlaying] = useState(false);
+    const video = useRef<HTMLVideoElement>(null);
+    const handlePlay = () => {
+        if (playing) {
+            video.current?.pause();
+            setPlaying(false);
+        }
+        if (!playing) {
+            video.current?.play();
+            setPlaying(true);
+        }
+    }
+    
     return (
-        <video src={videoSrc} controls={false} />
+        <div className={styles.wrapper}>
+            <video
+                ref={video}
+                className={styles.video}
+                src={videoSrc}
+                controls={false}
+            />
+            <button type='button' className={styles.playBtn} onClick={handlePlay}></button>
+        </div>
     )
 }
