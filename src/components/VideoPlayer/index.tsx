15b5cd5dda8fe28cd345959e854cebe9f09287df
Javi Villar <javier.villar@makingscience.com>
2023-02-27 11:33:56 +0100

    videoPlayer: stop video when is not visible

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,22 +1,49 @@
 import styles from './styles.module.css';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import useIntersectionObserver from '../../hooks/useIntersectionObserver'
 
 
 export default function VideoPlayer({src}: {src: string}) {
     const [playing, setPlaying] = useState(false);
     const video = useRef<HTMLVideoElement>(null);
     const playerBtn = useRef<HTMLButtonElement>(null);
+    const entry = useIntersectionObserver(video, { threshold: 0.2 });
 
     const handlePlay = () => {
-        if (playing) {
-            video.current?.pause();
-            playerBtn.current?.classList.remove(styles.hidden);
-        }
-        else {
-            video.current?.play();
-            playerBtn.current?.classList.add(styles.hidden);
-        }
+        if (playing)
+            pauseVideo();
+        else 
+            playVideo();
+
+    }
+
+    const playVideo = () => {
+        video.current?.play();
+        playerBtn.current?.classList.add(styles.hidden);
         setPlaying(!playing);
+    }
+
+    const pauseVideo = () => {
+        video.current?.pause();
+        playerBtn.current?.classList.remove(styles.hidden);
+        setPlaying(!playing);
+    }
+
+    const stopVideo = () => {
+        if (!video.current || !playerBtn.current)
+            return;
+
+        if (video.current.currentTime === 0 && !playing)
+            return;
+        
+        video.current.pause();
+        video.current.currentTime = 0;
+        playerBtn.current.classList.remove(styles.hidden);
+        setPlaying(!playing);
+    }
+
+    if (entry && !entry.isIntersecting && video.current) {
+        stopVideo();
     }
 
     return (
